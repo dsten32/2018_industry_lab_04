fe0769ff81ed1d2687e7c7b79cde64d99637646c
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-13 16:09:29 +1300

    starting the lecturers class

--- src/ictgradschool/industry/arrays/lecturers/Lecturer.java
+++ src/ictgradschool/industry/arrays/lecturers/Lecturer.java
@@ -10,27 +10,69 @@ public class Lecturer {
     
     public Lecturer(String name, int staffId, String[] papers, boolean onLeave) {
         // TODO Complete this constructor method
+        this.name=name;
+        this.staffId=staffId;
+        this.onLeave=onLeave;
+        this.papers = new String[papers.length];
+        for (int i = 0; i < papers.length; i++) {
+            this.papers[i]=papers[i];
+        }
     }
     
     // TODO Insert getName() method here
-    
+
+    public String getName() {
+        return name;
+    }
+
     // TODO Insert setName() method here
-    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     // TODO Insert getStaffId() method here
-    
+
+    public int getStaffId() {
+        return staffId;
+    }
+
     // TODO Insert setStaffId() method here
-    
+
+    public void setStaffId(int staffId) {
+        this.staffId = staffId;
+    }
+
     // TODO Insert getPapers() method here
-    
+
+    public String[] getPapers() {
+        return papers;
+    }
+
     // TODO Insert setPapers() method here
-    
+
+    public void setPapers(String[] papers) {
+        this.papers = papers;
+    }
+
     // TODO Insert isOnLeave() method here
-    
+
+    public boolean isOnLeave() {
+        return onLeave;
+    }
+
     // TODO Insert setOnLeave() method here
-    
+
+    public void setOnLeave(boolean onLeave) {
+        this.onLeave = onLeave;
+    }
+
     // TODO Insert toString() method here
     
     // TODO Insert teachesMorePapersThan() method here
+    public boolean teachesMorePapersThan(Lecturer other){
+        return other.papers.length < this.papers.length;
+    }
 
 }
 
