5e7232bd99998d9dab374963b75d2a9531319e11
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-13 15:14:09 +1300

    doing mobiles class

--- src/ictgradschool/industry/arrays/mobilephones/MobilePhone.java
+++ src/ictgradschool/industry/arrays/mobilephones/MobilePhone.java
@@ -3,36 +3,60 @@ package ictgradschool.industry.arrays.mobilephones;
 public class MobilePhone {
 
     // TODO Declare the 3 instance variables:
-    // brand
-    // model
-    // price
+    private String brand;
+    private String model;
+    private double price;
     
     public MobilePhone(String brand, String model, double price) {
-        // Complete this constructor method
+        this.brand=brand;
+        this.model=model;
+        this.price=price;
     }
 
     // TODO Uncomment these methods once the corresponding instance variable has been declared.
     /*public String getBrand() {
         return brand;
     }
-    
+
     public void setBrand(String brand) {
         this.brand = brand;
     }*/
     
     // TODO Insert getModel() method here
-    
+
+    public String getModel() {
+        return model;
+    }
+
     // TODO Insert setModel() method here
-    
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
     // TODO Insert getPrice() method here
-    
+
+    public double getPrice() {
+        return price;
+    }
+
     // TODO Insert setPrice() method here
-    
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     // TODO Insert toString() method here
-    
+
+    @Override
+    public String toString() {
+        return brand + " " + model + " " + " which cost $" + price;
+    }
+
     // TODO Insert isCheaperThan() method here
     
     // TODO Insert equals() method here
+
 
 }
 
