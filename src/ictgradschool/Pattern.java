2e9bdbcd4b46ee0245d711fc6b6a02886b637f69
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-13 13:52:07 +1300

    filled instance variables, creator method, getnumber and setnumber methods, looking at tostring method

--- src/ictgradschool/Pattern.java
+++ src/ictgradschool/Pattern.java
@@ -1,5 +1,24 @@
 package ictgradschool;
 
 public class Pattern {
+    private int numberOfCharacters;
+    private char character;
 
+    public Pattern(int num, char character){
+        this.numberOfCharacters=num;
+        this.character=character;
+    }
+
+    public int getNumberOfCharacters(){
+        return numberOfCharacters;
+    }
+
+    public void setNumberOfCharacters(int num){
+        numberOfCharacters=num;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
