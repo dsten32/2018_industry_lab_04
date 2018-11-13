9485d280ba6f9a013cf9313beba4d9bfaf876918
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-14 08:45:19 +1300

    doing print director one, adding the not in collection bit. I hope

--- src/ictgradschool/industry/arrays/movies/MovieProgram.java
+++ src/ictgradschool/industry/arrays/movies/MovieProgram.java
@@ -12,7 +12,7 @@ public class MovieProgram {
         Movie mostRecentMovie = getMostRecentMovie(films);      
         Movie longestMovie = getLongestMovie(films);       
         
-        printResults(mostRecentMovie, longestMovie); 
+        //printResults(mostRecentMovie, longestMovie);
         System.out.println();
         
         printDirector("Searching for Sugar Man", films);
@@ -74,10 +74,15 @@ public class MovieProgram {
     
     private void printDirector(String movieName, Movie[] movies) {
         // TODO Step 5. Complete the printDirector() method
+        String directedBy = "";
         for (int i = 0; i < movies.length; i++) {
             if (movies[i].getName() == movieName){
-                System.out.println(movieName + " was directed by " + movies[i].getDirector());
+                directedBy = movieName + " was directed by " + movies[i].getDirector();
             }
+            if (directedBy.length()==0){
+                directedBy=movieName + " is not in the collection.";
+            }
+            System.out.println(directedBy);
         }
     }
 
